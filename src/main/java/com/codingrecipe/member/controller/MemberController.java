fffbc419d8a1b2677932a551068078b47d96857a
kim woo hee <rladngml2002@naver.com>
2024-04-07 18:12:58 +0900

    login 기능 추가

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.codingrecipe.member.controller;
 
 import com.codingrecipe.member.dto.MemberDTO;
 import com.codingrecipe.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,6 @@ public class MemberController {
     //생성자 주입
     private final MemberService memberService;
 
-
-
     //회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
@@ -29,5 +28,21 @@ public class MemberController {
         System.out.println("memberDTO = "+memberDTO);
         memberService.save(memberDTO);
         return "index";
+    }
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+        MemberDTO loginResult= memberService.login(memberDTO);
+        if(loginResult!=null){
+            //login 성공
+            session.setAttribute("loginEmail",loginResult.getMemberEmail());
+            return "main";
+        }else{
+            //login 실패
+            return "login";
+        }
+    }
+    @GetMapping("/member/login")
+    public String loginFrom(){
+        return "login";
     }
 }
--- src/main/java/com/codingrecipe/member/entity/MemberEntity.java
+++ src/main/java/com/codingrecipe/member/entity/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.codingrecipe.member.entity;
 
+import com.codingrecipe.member.dto.MemberDTO;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,5 +18,16 @@ public class MemberEntity {
     private String memberEmail;
 
     @Column
+    private String memberPassword;
+
+    @Column
     private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        return memberEntity;
+    }
 }
