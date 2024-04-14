163cefe9ef1a0d123bd952d83c137d6bbd55febb
kim woo hee <rladngml2002@naver.com>
2024-04-14 16:02:30 +0900

    상세 정보 조회 구현

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -5,10 +5,10 @@ import com.codingrecipe.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,5 +44,19 @@ public class MemberController {
     @GetMapping("/member/login")
     public String loginFrom(){
         return "login";
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model){
+       List<MemberDTO> memberDTOList = memberService.findAll();
+       // 어떠한 html로 가져갈 데이터가 있다면 model 사용 (model을 사용하는건 가장 기본적인 방법)
+       model.addAttribute("memberList", memberDTOList);
+       return "list";
+    }
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id, Model model){
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member",memberDTO);
+        return "detail";
     }
 }
--- src/main/java/com/codingrecipe/member/service/MemberService.java
+++ src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -6,6 +6,8 @@ import com.codingrecipe.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -40,6 +42,24 @@ public class MemberService {
             }
         }else{
             //조회 결과가 없다(해당 이메일을 가진 회원 정보가 없다)
+            return null;
+        }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for(MemberEntity memberEntity : memberEntityList){
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+        }
+        return memberDTOList;
+    }
+
+    public MemberDTO findById(Long id) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+        if(optionalMemberEntity.isPresent()){
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        }else{
             return null;
         }
     }
